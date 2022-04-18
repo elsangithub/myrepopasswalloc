9473e443275ffd9d04c76b7edfcbe223f4e11bbd
elsangithub <102652357+elsangithub@users.noreply.github.com>
2022-04-18 20:25:37 +0900

    Update Uji.php

--- Openclash/Dev/core-lateset/Uji.php
+++ Openclash/Dev/core-lateset/Uji.php
@@ -1,1 +1,3 @@
 
+src/gz packpasswall https://mirrors.tuna.tsinghua.edu.cn/osdn/storage/g/o/op/openwrt-passwall-build/snapshots/packages/aarch64_cortex-a53/passwall_packages/
+src/gz lucipasswall https://mirrors.tuna.tsinghua.edu.cn/osdn/storage/g/o/op/openwrt-passwall-build/snapshots/packages/aarch64_cortex-a53/passwall_luci/
